cd6861566411a2190dadd022af7e12650d819d63
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-22 23:25:36 +0530

    feat: add getWeatherFromCity function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const getWeatherFromCity = async (city) => {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=48df9710e82d465da64214434232006&q=${city}`,
+    {
+      mode: "cors",
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+    });
+};
+
+getWeatherFromCity("New Delhi");
