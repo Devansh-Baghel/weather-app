4944ea04eb246f3bee2a442cda18c1bf5eceb865
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-24 13:37:26 +0530

    feat: add initial page load function

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,4 @@
+import { doc } from "prettier";
 import {
 	IP,
 	getWeatherFromCity
@@ -9,8 +10,8 @@ const cityFromIp = IP.getCity();
 const countryFromIp = IP.getCountry();
 const rainFromIp = `${IP.getRain()}cm`
 const windFromIp = `${IP.getWind()}km/h`
-const celsiusFromIp = `${IP.getTempCelsius()}°C`
-const fahrenheitFromIp = `${IP.getTempFahrenheit()}°F`
+const celsiusFromIp = IP.getTempCelsius()
+const fahrenheitFromIp = IP.getTempFahrenheit()
 
 // console.log(humidityFromIp, conditionFromIp, cityFromIp, countryFromIp, rainFromIp, windFromIp, celsiusFromIp, fahrenheitFromIp);
 
@@ -24,6 +25,35 @@ const windFromSearch = `${weatherFromSearch.current.wind_kph}km/h`
 const celsiusFromSearch = `${weatherFromSearch.current.temp_c}°C`
 const fahrenheitFromSearch = `${weatherFromSearch.current.temp_f}°F`
 
+
+const locationText = document.querySelector("#location-text");
+const dateText = document.querySelector("#date-text");
+const conditionImg = document.querySelector("#condition-img");
+const temprature = document.querySelector("#temprature");
+const conditionText = document.querySelector("#condition-text");
+const rainValue = document.querySelector("#rain-value");
+const windValue = document.querySelector("#wind-value");
+const humidityValue = document.querySelector("#humidity-value");
+
+
 // console.log(humidityFromSearch, conditionFromSearch, cityFromSearch, countryFromSearch, rainFromSearch, windFromSearch, celsiusFromSearch, fahrenheitFromSearch);
 
+const DOM = {
+	updateDisplayFromIp: () => {
+		locationText.innerHTML = `${cityFromIp},<br>${countryFromIp}`
+		let date = new Date();
+		date = date.toDateString();
+		date = date.slice(0, 10);
+		dateText.innerText = date;
+		// conditionImg.src = 
+		temprature.innerHTML = `${celsiusFromIp}<sup>°C</sup>`;
+		conditionText.innerText = conditionFromIp;
+		rainValue.innerText = rainFromIp;
+		windValue.innerText = windFromIp;
+		humidityValue.innerText = humidityFromIp;
+	}
+}
 
+export {
+	DOM
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { DOM } from "./dom";
+
+// Display the Weather on initial page load using user's ip
+DOM.updateDisplayFromIp();
