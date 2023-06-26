84dce1dbe9c62b2d7f91f46bd8d1ed0c2fbc31b2
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-26 11:57:25 +0530

    feat: user can now type "/" to focus on input field

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ const searchInput = document.querySelector("#search-input");
 // Display the Weather on initial page load using user's ip
 DOM.updateDisplayFromIp();
 
+// Focus on input field on inital page load
+searchInput.focus();
+
+window.addEventListener("keypress", (e) => {
+  if (e.key === "/") {
+    e.preventDefault();
+    searchInput.focus();
+  }
+})
+
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   if (searchInput.value === "") return;
