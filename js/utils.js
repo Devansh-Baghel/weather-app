2df054b0789d7e2f93fc8f0e79df1effe4dbea38
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-27 09:04:54 +0530

    fix: will this work?

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 const getWeatherFromCity = async (city) => {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=48df9710e82d465da64214434232006&q=${city}&days=4`,
+    `https://api.weatherapi.com/v1/forecast.json?key=48df9710e82d465da64214434232006&q=${city}&days=4`,
     { mode: "cors" }
   );
   const data = await response.json();
@@ -10,7 +10,7 @@ const getWeatherFromCity = async (city) => {
 const IP = {
   getIp: async () => {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/ip.json?key=48df9710e82d465da64214434232006&q=auto:ip`,
+      `https://api.weatherapi.com/v1/ip.json?key=48df9710e82d465da64214434232006&q=auto:ip`,
       { mode: "cors" }
     );
     const data = await response.json();
@@ -20,7 +20,7 @@ const IP = {
   getWeather: async () => {
     const ip = await IP.getIp();
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=48df9710e82d465da64214434232006&q=${ip}&days=4`,
+      `https://api.weatherapi.com/v1/forecast.json?key=48df9710e82d465da64214434232006&q=${ip}&days=4`,
       { mode: "cors" }
     );
     const data = await response.json();
